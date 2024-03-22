bd256fe9ca5dddc723c17698224d8eb5c95a5581
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2024-03-22 22:38:58 +0530

    saving functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# stitching_shop 😄 Trying to create a working website by learning new things 😍  check it out ... https://stitchingshop.netlify.app/
+# stitching_shop 😄 Trying to create a working website by learning new things 😍
+### It will give you a great experience .
+[check it out ...](https://stitchingshop.netlify.app/)
--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,40 +1,87 @@
-function menuitemfun() {
-  let menuItemArray = [
-    { itemName: " Luca Faloni Portofino Linen Shirt", itemimg: "https://www.apetogentleman.com/wp-content/uploads/2023/06/ArtisanalMenswear3.jpg" },
-    { itemName: "Blugiallo Jeans Raw Denim 14oz Selvedge", itemimg: "https://www.apetogentleman.com/wp-content/uploads/2023/06/ArtisanalMenswear10.jpg" },
-    { itemName: "Blugiallo Trouser Cotton & Linen Olive Green", itemimg: "https://www.apetogentleman.com/wp-content/uploads/2023/06/ArtisanalMenswear12.jpg" },
-    { itemName: "Long sleeve shirts", itemimg: "https://www.othingstodo.com/wp-content/uploads/2022/07/Long-sleeve-shirts-traditonal-dress-of-haryana.jpg" },
-  ];
-  menuItemArray.forEach((item,index,error) => {
-    let menuitembox = document.querySelector(".menuitembox");
-    let menuitems = document.createElement("div");
-    menuitems.classList.add("menuitems");
-    menuitems.innerHTML = `
-<div class="productimg menuitemimg">
-  <img src="${item.itemimg}" />
-</div>
-<h4 class="menuitemname productname">${item.itemName}</h4>
-<div class="btnsMenu">
-  <button class="menuitemdetails productdetails">Details</button>
-  <button class="addbtn">Add</button>
-</div>`;
-    menuitembox.prepend(menuitems);
+let menuItemArray = [
+  {
+    itemName: "Luca Faloni Portofino Linen Shirt",
+    itemimg:
+      "https://www.apetogentleman.com/wp-content/uploads/2023/06/ArtisanalMenswear3.jpg",
+      price:1000,
+      category: "Male"
+  },
+  {
+    itemName: "Blugiallo Jeans Raw Denim 14oz Selvedge",
+    itemimg:
+      "https://www.apetogentleman.com/wp-content/uploads/2023/06/ArtisanalMenswear10.jpg",
+      price:1000,
+      category: "Male"
+  },
+  {
+    itemName: "Blugiallo Trouser Cotton & Linen Olive Green",
+    itemimg:
+      "https://www.apetogentleman.com/wp-content/uploads/2023/06/ArtisanalMenswear12.jpg",
+      price:1000,
+      category: "Male"
+  },
+  {
+    itemName: "Long sleeve shirts",
+    itemimg:
+      "https://www.othingstodo.com/wp-content/uploads/2022/07/Long-sleeve-shirts-traditonal-dress-of-haryana.jpg",
+      price:1000,
+      category: "Male"
+  },
+];
+
+let menuitembox = document.querySelector(".menuitembox");
+
+function setMenuItems() {
+  menuItemArray.forEach((item, index) => {
+    let menuitems = `<div class="menuitems">
+      <div data-index="${index}" class="productimg menuitemimg">
+        <img src="${item.itemimg}" data-index="${index}" />
+      </div>
+      <h4 data-index="${index}" class="menuitemname productname">${item.itemName}</h4>
+      <div class="btnsMenu">
+        <button data-index="${index}" class="menuitemdetails productdetails">Details</button>
+        <button data-index="${index}" class="savebtn">Save item</button>
+      </div>
+    </div>`;
+    menuitembox.innerHTML += menuitems;
   });
-
-  function menuclick(){
-    let item = document.querySelectorAll('.menuitems')
-    item.forEach((elem,index)=>{
-      elem.addEventListener("click",(evt)=>{
-        console.log(evt.target);
-        let clicked = evt.target;
-    });
-      })
-    }
-   
-  
-  menuclick();
 }
-menuitemfun();
+
+setMenuItems();
+
+let savedItems = [];
+function showDetails(obj){
+  let cardbox = document.querySelector('.cardContainer');
+  let card = `
+  <div id="card">
+  <img src="${obj.itemimg}">
+  <h1> Category :  ${obj.category}</h1>
+  <p>Item Name : ${obj.itemName}</p>
+  <h5>Price : ${obj.price}<i class="ri-money-rupee-circle-line"></i></h5>
+  <button id="contect"><a href="https://wa.me/9728541543" target="_blank">Contect now &nbsp<i style="color:white;" class="ri-whatsapp-line"></i></a></button>
+  <a href="../../index.html#menuSec" class='backbtn'> Back &nbsp; <i class="ri-home-smile-fill"></i></a>
+  </div>
+  `;
+  cardbox.innerHTML = card;
+  cardbox.style.left = 0;
+  cardbox.style.zIndex =99 ;
+  let backbtn = cardbox.querySelector(".backbtn");
+  backbtn.addEventListener("click",(e)=>{
+    cardbox.innerHTML = null;
+  cardbox.style.left = "-100%";
+  cardbox.style.zIndex =-99 ;
+  })
+}
 
 
+function saveItemFun(evt) {
+  let dataIndex = evt.target.getAttribute("data-index");
+  if (evt.target.classList.contains("savebtn")) {
+    savedItems.push(menuItemArray[dataIndex]);
+  }
+  else {
+    showDetails(menuItemArray[dataIndex])
+  }
+}
 
+menuitembox.addEventListener("click", saveItemFun);
